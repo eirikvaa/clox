1952654574e9f7b74293d32bdbabbb7fe22487ac
Eirik Vale Aase <e.v.aase@gmail.com>
2018-05-28 07:33:22 +0200

    Finish chapter 16

---
+++ compiler.h
@@ -0,0 +1,10 @@
+//
+// Created by Eirik Vale Aase on 19.05.2018.
+//
+
+#ifndef clox_compiler_h
+#define clox_compiler_h
+
+void compile(const char *source);
+
+#endif //clox_compiler_h
--- vm.c
+++ vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -91,10 +92,7 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-
-    InterpretResult result = run();
-    return result;
+InterpretResult interpret(const char *source) {
+    compile(source);
+    return INTERPRET_OK;
 }
